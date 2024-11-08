6b39e1accb571de4779aa281f062117cdedc675a
barnii77 <adrunkenguy@gmx.at>
2024-11-08 09:20:04 +0100

    adjusted bounce back limits once more

--- src/htlstp_tdot/app.py
+++ src/htlstp_tdot/app.py
@@ -855,7 +855,7 @@ sim = market.MarketSimMix(
     (
         market.MarketSim(  # this sim provides high frequency "day trading" dynamics that can be changed by fake X posts etc
             base_value=100.0,
-            bounce_back_value=70.0,
+            bounce_back_value=60.0,
             market_boost_increase_factor=1.0,
             seed=seed,
             stddev=0.005,
