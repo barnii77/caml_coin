4270d86e4d7c9d312cfa2bf6825f0de7ef16fbb6
barnii77 <adrunkenguy@gmx.at>
2024-11-08 09:05:58 +0100

    made menu size adaptive to available space on screen

--- src/htlstp_tdot/app.py
+++ src/htlstp_tdot/app.py
@@ -894,7 +894,7 @@ sim = market.MarketSimMix(
         ),
         market.MarketSim(  # this simulation provides low frequency hourly / 20-minute trends that must not be influenced by events
             base_value=0,
-            bounce_back_value=-9999999999999999,
+            bounce_back_value=-50,
             market_boost_increase_factor=1.0,
             seed=seed,
             stddev=0.06,
