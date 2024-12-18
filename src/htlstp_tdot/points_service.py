ef2915bd0407a8d694d31ec9d049987bf798db2a
barnii77 <adrunkenguy@gmx.at>
2024-12-18 22:31:19 +0100

    lots of old accounts have invalid emails, so I can't do verification of email when logging in or creating a token. I can only check email validity on signup

--- src/htlstp_tdot/points_service.py
+++ src/htlstp_tdot/points_service.py
@@ -319,8 +319,8 @@ def login():
     state = request.args.get("state")
     callback_uri = request.args.get("redirect_uri")
     email = request.form.get("email")
-    if not verify_email(email):
-        return jsonify(error="Invalid email"), 400
+    # if not verify_email(email):
+    #     return jsonify(error="Invalid email"), 400
     password = request.form.get("password")
     out = get_user_id_from_email(email)
     if "error" in out:
@@ -378,8 +378,8 @@ def get_login_token():
     if request.method == "GET":
         return render_template("points_service_get_token.html")
     email = request.form.get("email")
-    if not verify_email(email):
-        return jsonify(error="Invalid email"), 400
+    # if not verify_email(email):
+    #     return jsonify(error="Invalid email"), 400
     password = request.form.get("password")
     out = get_user_id_from_email(email)
     if "error" in out:
